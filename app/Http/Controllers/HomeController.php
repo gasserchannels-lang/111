2ab42e4e61dd11e9ae06e21ed3543e07d1f79174
gasseraly <gasser.elshewaikh@gmail.com>
2025-09-06 02:37:53 +0300

    fix: Correct syntax error in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,6 @@ class HomeController extends Controller
     public function index()
     {
         // ابدأ بكود بسيط جدًا لضمان عمله
-        return view('welcome')
+        return view('welcome');
     }
 }
