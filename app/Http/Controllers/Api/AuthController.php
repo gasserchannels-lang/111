3b5b6d6958c061987264b0cd999d8109bf06cf6b
gasserchannels-lang <232266661+gasserchannels-lang@users.noreply.github.com>
2025-09-19 20:42:09 +0000

    style: Automatically fix code style issues

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
